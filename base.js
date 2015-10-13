aa6434a2f59a92b1da4928e317c57f82f03f52cb
danielle <dani.zaghian@gmail.com>
2015-10-12 19:16:19 -0700

    counter feature added

--- base.js
+++ base.js
@@ -2,17 +2,28 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function () {
 
+	// find starting number of posts, append to count div
+	var countPosts = 0;
+
 	//grab the value from the input box
     $('form').on('submit', function(e) {
     	e.preventDefault();
-        var toPost = $('#postItem').val();
-        //on submit, append posted item to page in UL
+        var toPost = $('#inputPost').val();
+        //on submit, append posted item to UL
         $('ul').append('<li>' + toPost + '</li>');
-        //clear text in text bos
-        $('form').children('#postItem').val('');
+        //clear text in form once submitted
+        $('#inputPost').val('');
+        //increase post count and append to count div
+		countPosts = countPosts + 1;
+		$("#counter").text(" ").append(countPosts + " posts");
     });
-    //when you click the item, remove from list
+    //when you click the element, remove from list
     $(document).on('click', 'li', function () {
         $(this).remove();
     });
+
+   
+
+    
+
 });
