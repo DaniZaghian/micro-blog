3ca7c26975fba3c8266db5462d34d3bf38639468
danielle <dani.zaghian@gmail.com>
2015-10-12 18:32:39 -0700

    initial commit

--- base.js
+++ base.js
@@ -1,7 +1,18 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // code in here
-
+	//grab the value from the input box
+    $('form').on('submit', function(e) {
+    	e.preventDefault();
+        var toPost = $('#postItem').val();
+        //on submit, append posted item to page in UL
+        $('ul').append('<li>' + toPost + '</li>');
+        //clear text in text bos
+        $('form').children('#postItem').val('');
+    });
+    //when you click the item, remove from list
+    $(document).on('click', 'li', function () {
+        $(this).remove();
+    });
 });
