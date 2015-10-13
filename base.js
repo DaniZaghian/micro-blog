261eaa7e9afabcd8b86b689ac261268d0fceb9f3
danielle <dani.zaghian@gmail.com>
2015-10-12 20:56:32 -0700

    added bootstrap styles and images

--- base.js
+++ base.js
@@ -9,21 +9,30 @@ $(document).ready(function () {
     $('form').on('submit', function(e) {
     	e.preventDefault();
         var toPost = $('#inputPost').val();
+
         //on submit, append posted item to UL
-        $('ul').append('<li>' + toPost + '</li>');
+        $('ul').append('<li class="list-group-item">' + toPost + '</li>');
+
         //clear text in form once submitted
         $('#inputPost').val('');
+
         //increase post count and append to count div
 		countPosts = countPosts + 1;
-		$("#counter").text(" ").append(countPosts + " posts");
+		$("#counter").text(" ").append("Posts: " + countPosts);
+   
     });
+
     //when you click the element, remove from list
     $(document).on('click', 'li', function () {
         $(this).remove();
+        countPosts = countPosts - 1;
+		$("#counter").text(" ").append("Posts: " + countPosts);
     });
 
-   
 
-    
+    //tried using affix img but found float left was easier
+ 	//$('img').affix-bottom({
+ 	//offset: 1
+	//});
 
 });
